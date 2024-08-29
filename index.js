e453eb54d968fdce09275230e65fbd6484296703
Ankit Kumar <darkwebcode68@gmail.com>
2024-08-29 10:28:27 +0000

    main.file

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,10 +15,46 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+const originalUrls = [];
+const shortUrls = [];
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  const foundIndex = originalUrls.indexOf(url);
+
+  if (!url.includes("https://") && !url.includes("http://")) {
+    return res.json({ error: 'invalid url' })
+  }
+
+  if (foundIndex < 0) {
+    originalUrls.push(url);
+    shortUrls.push(shortUrls.length);
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length -1
+    })
+  }
+
+  res.json({ 
+    original_url: url,
+    short_url: shortUrls[foundIndex]
+   });
 });
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const shorturl = parseInt(req.params.shorturl);
+  const foundIndex = shortUrls.indexOf(shorturl);
+
+  if (foundIndex < 0) {
+    res.json({
+      error: "No short URL found for the given input"
+    })
+  }
+  res.redirect(originalUrls[foundIndex])
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
